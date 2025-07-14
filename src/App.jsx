66eff0441cc64b14e800387eeff478de45658532
Juaan26 <Juaan26g@gmail.com>
2025-07-14 05:46:19 +0200

    estructura basica del proyecto

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
-import { useState } from 'react'
-
 import './App.css'
-
+import Home from "./pages/Home"
 function App() {
 
 
+
+
   return (
-    <>
-      <h1>Panel principal</h1>
-    </>
+    <Routes>
+      <Route path="/" element={ <Home /> } />
+    </Routes>
   )
 }
 
